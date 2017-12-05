1de845957b96727439d88275fa5bdcce31c2e674
ganlin <gan_lin@ymsl.com.cn>
2017-12-05 18:17:58 +0800

    update

--- src/js/vue-aside.js
+++ src/js/vue-aside.js
@@ -9,7 +9,7 @@
 })(this, function(Vue, VuePopup, VueUtil) {
 	'use strict';
 	var VueAside = {
-		template: '<div :class="[{\'vue-aside__static\':relative}]"><div v-show="visibleaside" :class="[\'vue-aside__wrapper\', {\'vue-aside__absolute\':relative}, {\'is-clear\': clearModal}]" @click.self="handleWrapperClick"></div><transition :name="left ? \'aside-left\' : \'aside-right\'"><div v-show="visibleaside" :class="[\'vue-aside\', {\'vue-aside-left\':left, \'vue-aside__absolute\':relative},sizeClass,customClass]" ref="aside"><div v-if="showClose" class="vue-aside__headerbtn"><i class="vue-aside__close vue-icon vue-icon-close" @click=\'handleClose\'></i></div><div class="vue-aside__header"><span class="vue-aside__title" v-if="showTitle && !$slots.header">{{title}}</span><slot name="header"></slot></div><div class="vue-aside__body"><slot></slot></div><div class="vue-aside__footer" v-if="$slots.footer"><slot name="footer"></slot></div></div></transition></div>',
+		template: '<div :class="[{\'vue-aside__static\':relative}]"><div v-show="visibleaside" :class="[\'vue-aside__wrapper\', {\'vue-aside__absolute\':relative}, {\'is-cleanness\': cleannessModal}]" @click.self="handleWrapperClick"></div><transition :name="left ? \'aside-left\' : \'aside-right\'"><div v-show="visibleaside" :class="[\'vue-aside\', {\'vue-aside-left\':left, \'vue-aside__absolute\':relative},sizeClass,customClass]" ref="aside"><div v-if="showClose" class="vue-aside__headerbtn"><i class="vue-aside__close vue-icon vue-icon-close" @click=\'handleClose\'></i></div><div class="vue-aside__header"><span class="vue-aside__title" v-if="showTitle && !$slots.header">{{title}}</span><slot name="header"></slot></div><div class="vue-aside__body"><slot></slot></div><div class="vue-aside__footer" v-if="$slots.footer"><slot name="footer"></slot></div></div></transition></div>',
 		name: 'VueAside',
 		mixins: [VuePopup],
 		data: function(){
@@ -38,7 +38,7 @@
 				type: String,
 				default: ""
 			},
-			clearModal: Boolean,
+			cleannessModal: Boolean,
 			beforeClose: Function
 		},
 		watch: {
