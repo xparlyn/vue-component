e284dde6d8050a88dca21bfee746b1754021eaf3
ganlin <gan_lin@ymsl.com.cn>
2018-01-26 11:01:58 +0800

    update

--- src/js/vue-util.js
+++ src/js/vue-util.js
@@ -11,7 +11,7 @@
 	}
 })(this, function(Vue, SystemInfo, DateUtil) {
 	'use strict';
-	var version ='1.36.9311';
+	var version ='1.36.9318';
 	var isDef = function(v) {
 		return v !== undefined && v !== null
 	};
