76d5f54bbdd44f6d729b2b51291149ba72b7079f
ganlin <gan_lin@ymsl.com.cn>
2018-03-15 18:34:59 +0800

    update

--- src/js/vue-img-load.js
+++ src/js/vue-img-load.js
@@ -9,17 +9,6 @@
 })(this, function(Vue, VueUtil) {
 	'use strict';
 	var imgload = function() {
-		var fadeIn = function(el) {
-			el.style.opacity = 0;
-			el.style.display = "block";
-			VueUtil.debounce(function _fadeIn() {
-				var val = parseFloat(el.style.opacity);
-				if (!((val += .1) > 1)) {
-					el.style.opacity = val;
-					_fadeIn();
-				}
-			})();
-		};
 		Vue.directive('imgload', {
 			bind: function(el, binding) {
 				if (el.tagName === 'IMG') {
@@ -27,11 +16,10 @@
 					img.src = binding.value;
 					img.onload = function() {
 						el.src = img.src;
-						fadeIn(el);
 					}
 				}
 			}
-		})
+		});
 	};
 	Vue.use(imgload);
 });
