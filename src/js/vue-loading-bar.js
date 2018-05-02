55bbc4a3aba57437b5796e5576dad98794ba3ea6
ganlin <gan_lin@ymsl.com.cn>
2018-05-02 17:26:38 +0800

    update

--- src/js/vue-loading-bar.js
+++ src/js/vue-loading-bar.js
@@ -8,39 +8,29 @@
 	}
 })(this, function(Vue, VueUtil) {
 	'use strict';
-	var loadingBarInstance = null;
+	var loadingBar = null;
 	var intervaler = null;
 	var LoadingBar = Vue.extend({
-		template: '<transition @after-leave="handleAfterLeave"><div v-show="visible" class="vue-loading-bar"><div :class="innerClasses" :style="styles"></div></div></transition>',
+		template: '<div v-show="visible" class="vue-loading-bar"><div :class="innerClasses" :style="innerStyle"></div></div>',
 		data: function() {
 			return {
 				percent: 0,
-				status: '',
+				error: false,
 				visible: false
 			};
 		},
+		watch: {
+			visible: function(val) {
+				if (val) this.$el.style.zIndex = VueUtil.nextZIndex();
+			}
+		},
 		computed: {
 			innerClasses: function() {
-				return ['vue-loading-bar-inner', 'vue-loading-bar-inner-color-primary', {'vue-loading-bar-inner-color-error': this.status === 'error'}];
+				return ['vue-loading-bar-inner', 'vue-loading-bar-inner-color-primary', {'vue-loading-bar-inner-color-error': this.error}];
 			},
-			styles: function() {
-				var style = {
-					width: this.percent + '%',
-					height: '2px'
-				};
-				return style;
+			innerStyle: function() {
+				return {width: this.percent + '%'};
 			}
-		},
-		methods: {
-			handleAfterLeave: function() {
-				this.$destroy();
-				this.$nextTick(function(){
-					loadingBarInstance = null;
-				});
-			}
-		},
-		mounted: function() {
-			this.$el.style.zIndex = VueUtil.nextZIndex();
 		}
 	});
 	var newInstance = function() {
@@ -48,55 +38,59 @@
 		document.body.appendChild(div);
 		var loadingBar = new LoadingBar({el: div});
 		return {
-			update: function(options) {
-				loadingBar.visible = true;
+			show: function(options) {
+				if (!loadingBar.visible) loadingBar.visible = true;
+				loadingBar.error = options.error;
 				if (VueUtil.isDef(options.percent)) loadingBar.percent = options.percent;
-				if (VueUtil.isDef(options.status)) loadingBar.status = options.status;
 			},
-			destroy: function() {
+			hide: function() {
 				loadingBar.visible = false;
+			},
+			isShow: function() {
+				return loadingBar.visible;
 			}
 		};
 	};
-	var updateInstance = function(options) {
-		if (!VueUtil.isDef(loadingBarInstance)) loadingBarInstance = newInstance();
-		loadingBarInstance.update(options);
+	var initLoadingBar = function() {
+		if (!VueUtil.isDef(loadingBar)) loadingBar = newInstance();
 	};
-	var destroyInstance = VueUtil.debounce(500, function(fn) {
-		if (VueUtil.isDef(loadingBarInstance)) {
-			if (VueUtil.isFunction(fn)) fn();
-			loadingBarInstance.destroy();
-		}
+	var hideInstance = VueUtil.debounce(500, function(fn) {
+		if (VueUtil.isFunction(fn)) fn();
+		loadingBar.hide();
 	});
 	var VueLoadingBar = {
 		start: function(fn) {
-			if (VueUtil.isDef(loadingBarInstance)) return;
+			initLoadingBar();
+			if (loadingBar.isShow()) return;
 			var percent = 0;
-			updateInstance({percent: percent});
+			loadingBar.show({percent: percent});
 			intervaler = setInterval(function() {
 				percent += 6;
 				if (percent > 95) {
 					clearInterval(intervaler);
 					percent = 96;
 				}
-				updateInstance({percent: percent});
-			}, 200);
+				loadingBar.show({percent: percent});
+			}, 250);
 			if (VueUtil.isFunction(fn)) fn();
 		},
 		update: function(percent, fn) {
+			initLoadingBar();
 			clearInterval(intervaler);
-			updateInstance({percent: percent});
+			loadingBar.show({percent: percent});
 			if (VueUtil.isFunction(fn)) fn();
 		},
 		finish: VueUtil.debounce(function(fn) {
+			initLoadingBar();
 			clearInterval(intervaler);
-			updateInstance({percent: 100});
-			destroyInstance(fn);
+			loadingBar.show({percent: 100});
+			hideInstance(fn);
 		}),
 		error: VueUtil.debounce(function(fn) {
+			initLoadingBar();
 			clearInterval(intervaler);
-			updateInstance({percent: 100, status: 'error'});
-			destroyInstance(fn);
+			loadingBar.show({percent: 100, error: true});
+			hideInstance(fn);
 		})
 	}
 	Vue.loadingBar = VueLoadingBar;
