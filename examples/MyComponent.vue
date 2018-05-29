ead318b7cd83f7ce7cb8c96b7a13d8005ce7b279
ganlin <gan_lin@ymsl.com.cn>
2018-05-29 19:33:04 +0800

    update

---
+++ examples/MyComponent.vue
@@ -0,0 +1,19 @@
+<style>
+.example {
+  color: red;
+}
+</style>
+
+<template>
+  <div class="example">{{ msg }}</div>
+</template>
+
+<script>
+module.exports = {
+  data () {
+    return {
+      msg: "Hello world!"
+    }
+  }
+}
+</script>
